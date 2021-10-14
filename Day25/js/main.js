67650c89d6b9d28a4939b7b2436a049bcf411164
Purva <gautam.purva@gmail.com>
2021-10-14 22:38:25 +0530

    basic code of day25

---
+++ Day25/js/main.js
@@ -0,0 +1,15 @@
+//General UI added
+document.body.style.backgroundColor='#F0F0F0'
+
+let header=document.querySelector('h2')
+header.style.textAlign='center'
+header.style.fontFamily='verdana'
+header.style.color='ORANGE'
+header.style.fontSize='40px'
+
+let subtitle=document.querySelector('p','subtitle')
+subtitle.style.textAlign='center'
+subtitle.textContent='Currently, we have '+countries.length+' countries'
+subtitle.style.fontSize='20px'
+
+
